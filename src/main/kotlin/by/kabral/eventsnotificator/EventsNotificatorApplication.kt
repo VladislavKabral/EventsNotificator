49fe434da67471a327e4236245d2aba23a374838
Vladislav Kabral <mr.kabral@mail.ru>
2025-10-15 22:28:49 +0300

    feat: init project

---
+++ src/main/kotlin/by/kabral/eventsnotificator/EventsNotificatorApplication.kt
@@ -0,0 +1,11 @@
+package by.kabral.eventsnotificator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventsNotificatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventsNotificatorApplication>(*args)
+}
